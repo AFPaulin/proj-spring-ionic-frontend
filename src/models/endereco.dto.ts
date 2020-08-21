e63937fb56ed7eab919c0814604b12d874cf6b0f
Andre Faramilio Paulin <andrefaramilio121@gmail.com>
2020-08-21 14:31:10 -0300

    Atualização: - Incluido tela de pagamento,de confirmação do pedido e de escolha do endereço. - Salvado pedido no banco de dados. - Informação do pedido para o cliente.

---
+++ src/models/endereco.dto.ts
@@ -0,0 +1,14 @@
+import { CidadeDTO } from "./cidade.dto";
+
+
+export interface EnderecoDTO {
+    id : string;
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cep: string;
+    cidade: CidadeDTO;
+    
+
+}
